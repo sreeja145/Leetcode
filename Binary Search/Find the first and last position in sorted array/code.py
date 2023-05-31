489c2226d82298d7c8db7749dc5d6f152c32c250
Nimmagadda Pavana Sreeja <73514771+sreeja145@users.noreply.github.com>
2023-05-31 20:33:31 +0530

    Create code.py

---
+++ Binary Search/Find the first and last position in sorted array/code.py
@@ -0,0 +1,25 @@
+def searchRange(self, nums, target):
+        def binarysearch(nums, target, flag):
+            output=-1
+            
+            start= 0
+            end= len(nums)-1
+            
+            while start <= end :
+                mid = (start + end) // 2
+                
+                if nums[mid] > target :
+                    end = mid - 1
+                    
+                elif nums[mid] < target :
+                    start = mid + 1
+                
+                else:
+                    if flag is True:
+                        output=mid
+                        end= mid-1
+                    else:
+                        output= mid
+                        start= mid+1
+            return output
+        return [binarysearch(nums, target, True), binarysearch(nums, target, False)]
