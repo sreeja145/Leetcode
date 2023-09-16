0ca030928d8666c959c920a4d8a21fdfbede453b
Nimmagadda Pavana Sreeja <73514771+sreeja145@users.noreply.github.com>
2023-09-16 20:50:41 +0530

    Create Valid Mountain Array .py

---
+++ Arrays/Valid Mountain Array .py
@@ -0,0 +1,45 @@
+def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        if len(arr)<3:
+            return False
+        i=0
+        increasing=1
+        decreasing =0
+        while i<len(arr)-1:
+            if increasing!=0:
+                if arr[i]<arr[i+1]:
+                    i=i+1
+                    decreasing+=1
+                else:
+                    increasing=0
+                
+            else:
+                if arr[i]>arr[i+1]:
+                    i=i+1
+                else:
+                    return False
+        if increasing==1 or decreasing ==0:
+            return False
+        return True
+  """
+  Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Input: arr = [2,1]
+Output: false
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true """
